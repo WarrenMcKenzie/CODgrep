25efe9a40856773108d03d09f69e14012dea85af
Warren McKenzie <56005436+WarrenMcKenzie@users.noreply.github.com>
2022-04-28 11:27:02 -1000

    Update CODgrep.py
    No longer requires input of names manually, will instead pull names from the primary URL associated with the .cod file (as in the HTML frontend, not the .cif itself, which aren't always reliable for containing names).

--- CODgrep.py
+++ CODgrep.py
@@ -1,19 +1,32 @@
 import requests
 from urllib.parse import urljoin
 import urllib.request
+import numpy as np
 import os  
+import re
+import time
 
 CIFs = list(map(int, input("Enter the CIF numbers with spaces or line breaks between them: ").split()))
-idents = list(map(str, input("Enter the names of the files to be retrieved in the same order with spaces or line breaks between them: ").split()))
 for i, x in enumerate(CIFs):
     baseUrl = ['http://www.crystallography.net/cod/']
     cifNum = list(str(x))
     cifExt = ['.cif']
-    urlComp = baseUrl + cifNum + cifExt
-    finalUrl = "".join(urlComp)
-    #name of the file
-    #minName = input("What is the identity of {}.cif".format(x))
-    minName = idents[i]
+    urlComp = baseUrl + cifNum + cifExt     
+    finalUrl = "".join(urlComp)             # Assembles the final URL
+    
+    if np.size(CIFs) > 60                   # Flood control, per crystallography.net API
+        time.sleep(10)                      # documentation for SQL queries
+    
+    # Name is pulled from the .cif information page
+    
+    nameExt = ['.html']                     # The COD is set up so the difference is the extension
+    urlComp = baseUrl + cifNum + nameExt
+    finalUrlName = "".join(urlComp)
+    namePull = requests.get(finalUrlName, 'html.parser')
+    nameText = namePull.text # renders page
+    nameFlat = "".join(nameText.split())    # Makes everything a monolithic block of text
+    minName = re.search(r'Mineralname</th><td>(.*?)</td>', nameFlat).group(1) # Pulls the mineral name
+    
     numName = int(''.join(map(str, cifNum))) # concatenates cif number
     fileComp = minName + "_" + str(numName) + str(cifExt[0])
     finalName = "".join(fileComp)
